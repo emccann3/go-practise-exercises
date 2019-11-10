c8d7cc000effd6428477c1b843b9783cf451b9dc
Emma McCann <emma@emmas-air.home>
2019-11-10 20:03:58 +0000

    add files to github

---
+++ Variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial" //string
+	fmt.Println(a)
+
+	var b, c int= 1,2 //two ints on one line
+	fmt.Println(b, c)
+
+	var d = true //boolean
+	fmt.Println(d)
+
+	var e int //int initializes to 0
+	fmt.Println(e)
+
+	f:="apple" //string
+	fmt.Println(f)
+}
